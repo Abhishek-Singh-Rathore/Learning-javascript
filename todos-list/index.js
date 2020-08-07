c966920247de9c844c6e80351115320d5069a82d
Abhishek S. Rathore <abhisheksinghrathore622@gmail.com>
2020-08-07 22:53:16 +0530

    updated

--- todos-list/index.js
+++ todos-list/index.js
@@ -1,1 +1,11 @@
- 
+let form =  document.querySelector("form");
+//console.log(form);
+
+form.addEventListener("submit",(event)=>{
+    event.preventDefault()
+  //console.log(event.target)
+  let input = document.querySelector(".input");
+  let value = input.value;
+ //console.log(value);  
+ input.value="";     
+})
